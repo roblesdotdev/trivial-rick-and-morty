70f07e3d9e08d93ec540e8257dac815039dc79f1
roblesdotdev <httpsnip@gmial.com>
2023-03-03 00:42:36 -0300

    client/setup react project

---
+++ apps/client/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        minHeight: "95vh",
+      }}
+    >
+      <h1>Working...</h1>
+    </div>
+  );
+}
+
+export default App;
